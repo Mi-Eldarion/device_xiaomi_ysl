3a23275ccb9c7bdc7a85dedc4eae8e24c8681481
Abhinav Sohane <abhi@codeaurora.org>
2019-01-03 16:51:23 +0530

    QCamera2:Added and Enabled support for libion api's
    1) Added check for old ioctal call for ION.
    2) Disabled ION cache for now
    3) Added new libion api's.
    
    Change-Id: If80c04d3ae6e0c0ae032b3948015e91d22374344
    
     Conflicts:
    	camera/QCamera2/Android.mk
    	camera/QCamera2/HAL/QCameraMem.cpp
    	camera/QCamera2/HAL3/QCamera3Mem.cpp

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -89,9 +89,15 @@ LOCAL_C_INCLUDES := \
         $(call project-path-for,qcom-media)/mm-core/inc \
         $(TARGET_OUT_HEADERS)/mm-camera-lib/cp/prebuilt
 
+ifneq (,$(filter $(TRINKET),$(TARGET_BOARD_PLATFORM)))
+LOCAL_C_INCLUDES += \
+        system/core/libion/kernel-headers \
+        system/core/libion/include
+endif
+
 LOCAL_HEADER_LIBRARIES := camera_common_headers
 LOCAL_HEADER_LIBRARIES += display_headers
-LOCAL_HEADER_LIBRARIES += media_plugin_headers
+LOCAL_HEADER_LIBRARIES := media_plugin_headers
 LOCAL_HEADER_LIBRARIES += libandroid_sensor_headers
 LOCAL_HEADER_LIBRARIES += libcutils_headers
 LOCAL_HEADER_LIBRARIES += libsystem_headers
@@ -123,6 +129,9 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
+ifneq (,$(filter $(TRINKET),$(TARGET_BOARD_PLATFORM)))
+LOCAL_SHARED_LIBRARIES += libion
+endif
 ifeq ($(USE_DISPLAY_SERVICE),true)
 LOCAL_SHARED_LIBRARIES += android.frameworks.displayservice@1.0 libhidlbase libhidltransport
 else
